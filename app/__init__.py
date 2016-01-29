2c73cf1e3a08d73c2595d3a7efbaaf10c7a7fe3a
Jason Keung <jason.s.keung@gmail.com>
2016-01-28 19:57:59 -0500

    adding app folder

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
-from app import views, models
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,4 +13,6 @@ db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+from app import views, models
--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
 from app import db
+from hashlib import md5
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -19,11 +24,25 @@ class User(db.Model):
         return False
 
     def get_id(self):
-        return unicode(self.id)  # python 2
+        return unicode(self.id)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
-
 
 
 class Post(db.Model):
@@ -34,3 +53,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
